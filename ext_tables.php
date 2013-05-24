527a2c8c0b3978a2ab21f12042dbe8da6a7a2037
Stefan Isak <mail@stefanisak.com>
2013-05-24 18:44:36 +0200

    [TASK] Add flexform configuration and clearCacheRegistry

--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,16 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'FAL CouchDB Driver');
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['registeredDrivers']['fal_couchdb'] = array(
+	'class' => 'Tx_FalCouchdb_Driver_CouchDB',
+	'shortName' => 'FAL CouchDB',
+	'flexFormDS' => 'FILE:EXT:fal_couchdb/Configuration/FlexForms/CouchDB.xml',
+	'label' => 'CouchDB'
+);
+
+if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['tx_falcouchdb_cache'])) {
+    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['tx_falcouchdb_cache'] = array();
+}
+$TYPO3_CONF_VARS['FE']['eID_include']['falCouchdbClearRegistry'] = 'EXT:fal_couchdb/Classes/Ajax/ClearRegistry.php';
 
 ?>

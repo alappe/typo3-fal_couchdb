a1b3c87032dced5b15a79df458e4c0c265355c8d
Andreas Lappe <nd@off-pist.de>
2013-05-24 21:34:57 +0200

    [TASK] Remove tables and use localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,12 +3,20 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+/*
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['registeredDrivers']['fal_couchdb'] = array(
-	'class' => 'Tx_FalCouchdb_Driver_CouchDB',
+	'class' => 'Zimmer7\\FalCouchdb\\Driver\\CouchDB',
 	'shortName' => 'FAL CouchDB',
 	'flexFormDS' => 'FILE:EXT:fal_couchdb/Configuration/FlexForms/CouchDB.xml',
 	'label' => 'CouchDB'
 );
+ */
+
+/**
+ * @var \TYPO3\CMS\Core\Resource\Driver\DriverRegistry $registry
+ */
+$registry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\Resource\Driver\DriverRegistry');
+$registry->registerDriverClass('\Zimmer7\FalCouchdb\Driver\CouchDB', 'FAL CouchDB', 'CouchDB', 'FILE:EXT:fal_couchdb/Configuration/FlexForms/CouchDB.xml');
 
 if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['tx_falcouchdb_cache'])) {
     $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['tx_falcouchdb_cache'] = array();
